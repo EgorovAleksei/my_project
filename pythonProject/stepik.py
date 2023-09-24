c93d89fb44b4d5f6ec6f75901a39abf7d62c1f10
EgorovAleksei <141329137+EgorovAleksei@users.noreply.github.com>
2023-09-24 12:09:12 +0300

    main.htm

--- pythonProject/stepik.py
+++ pythonProject/stepik.py
@@ -1,25 +1,53 @@
-from jinja2 import Template
-#from jinja2.filters import escape
+from jinja2 import Template, Environment, FileSystemLoader
+
 from markupsafe import escape
 
+persons = [
+    {"name": "Алексей", "old": 18, "weight": 78.5},
+    {"name": "Николай", "old": 28, "weight": 82.3},
+    {"name": "Иван", "old": 33, "weight": 94.0}]
 
-data = '''{% raw %}Модуль jinja вместо
-определения {{ name }}
-подставляет соответствующее значение{% endraw %}'''
+def loadTP
 
 
 
-tm = Template(data)
-msg = tm.render(name='Федор')
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+
+tm = env.get_template('main.htm')
+msg = tm.render(users=persons)
 print(msg)
-print()
 
 
-link = '''В HTML-документе ссылка определяются так:
-<a href="#">Ссылка</a>'''
-
-msg = escape(link)
 
 
-print(msg)
+
+
+
+
+
+
+
+
+
+
+
+
+# data = '''{% raw %}Модуль jinja вместо
+# определения {{ name }}
+# подставляет соответствующее значение{% endraw %}'''
+#
+# tm = Template(data)
+# msg = tm.render(name='Федор')
+# print(msg)
+# print()
+#
+#
+# link = '''В HTML-документе ссылка определяются так:
+# <a href="#">Ссылка</a>'''
+#
+# msg = escape(link)
+#
+#
+# print(msg)
 
